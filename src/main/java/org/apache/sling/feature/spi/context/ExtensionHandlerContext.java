5a3ba945838c1548252f6f28fbb72b553c9a05a1
David Bosschaert <bosschae@adobe.com>
2020-08-13 09:13:39 +0100

    Add license headers

--- src/main/java/org/apache/sling/feature/spi/context/ExtensionHandlerContext.java
+++ src/main/java/org/apache/sling/feature/spi/context/ExtensionHandlerContext.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.sling.feature.spi.context;
 
 import org.apache.sling.feature.io.artifacts.ArtifactManager;
@@ -27,5 +45,9 @@ public interface ExtensionHandlerContext {
      */
     public void addConfiguration(final String pid, final String factoryPid, final Dictionary<String, Object> properties);
 
+    /**
+     * Obtain the artifact manager.
+     * @return The artifact manager to use.
+     */
     public ArtifactManager getArtifactManager();
 }
--- src/main/java/org/apache/sling/installer/factory/model/impl/BinaryArtifactExtensionHandler.java
+++ src/main/java/org/apache/sling/installer/factory/model/impl/BinaryArtifactExtensionHandler.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.sling.installer.factory.model.impl;
 
 import org.apache.felix.utils.manifest.Clause;
@@ -11,8 +29,6 @@ import org.apache.sling.feature.spi.context.ExtensionHandlerContext;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -23,7 +39,6 @@ public class BinaryArtifactExtensionHandler implements ExtensionHandler {
     private static final String BINARY_EXTENSIONS_PROP = "org.apache.sling.feature.binary.extensions";
 
     private final Map<String, Map<String, String>> binaryExtensions;
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Activate
     public BinaryArtifactExtensionHandler(BundleContext bc) {
